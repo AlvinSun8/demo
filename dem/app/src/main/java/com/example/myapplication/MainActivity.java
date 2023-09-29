3349e0595f7f4c7ec579ff6cb369353cb2f359a0
Alvin Sun <1950367999@qq.com>
2023-09-29 20:03:05 +0800

    0929

--- dem/app/src/main/java/com/example/myapplication/MainActivity.java
+++ dem/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         Log.d("code", "this is test");
         TextView tv = findViewById(R.id.tb);
         tv.setText("非凡大师");
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                //跳转到Mainactivity2,packageContext是当前上下文
+                intent.setClass(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
